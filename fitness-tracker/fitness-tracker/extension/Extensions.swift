2e36cee7a4931ea7c5f526aaca06ca3dbb7231e0
Delvin Lim <delvinlim6414@gmail.com>
2025-09-14 05:57:16 +0800

    feature: added extensions to help in route/checkpoint calculations involving coordinates

--- fitness-tracker/fitness-tracker/extension/Extensions.swift
+++ fitness-tracker/fitness-tracker/extension/Extensions.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import SceneKit
+import CoreLocation
+import MapKit
 
 extension FloatingPoint {
     func clamped(to range: ClosedRange<Self>) -> Self {
@@ -35,5 +37,40 @@ extension SCNVector3 {
     }
     var length: Float {
         sqrt(x*x + y*y + z*z)
+    }
+}
+
+extension Notification.Name {
+    static let spawnCheckpoint = Notification.Name("spawnCheckpoint")
+}
+
+extension CLLocationCoordinate2D {
+    /// Calculates the initial bearing (direction) from this coordinate to another, in radians.
+    /// Bearing is measured clockwise from north (0 = north, π/2 = east, etc.).
+    func bearing(to destination: CLLocationCoordinate2D) -> Double {
+        let lat1 = latitude.degreesToRadians
+        let lon1 = longitude.degreesToRadians
+        let lat2 = destination.latitude.degreesToRadians
+        let lon2 = destination.longitude.degreesToRadians
+        
+        let dLon = lon2 - lon1
+        
+        let y = sin(dLon) * cos(lat2)
+        let x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(dLon)
+        
+        return atan2(y, x)  // Returns value in -π...π radians
+    }
+}
+
+// MARK: - Helper for Degrees/Radians Conversion
+extension CLLocationDegrees {  // CLLocationDegrees is a typealias for Double
+    var degreesToRadians: Double { self * .pi / 180.0 }
+    var radiansToDegrees: Double { self * 180.0 / .pi }
+}
+
+extension MKMapRect {
+    var centerCoordinate: CLLocationCoordinate2D {
+        let point = MKMapPoint(x: midX, y: midY)
+        return point.coordinate
     }
 }
