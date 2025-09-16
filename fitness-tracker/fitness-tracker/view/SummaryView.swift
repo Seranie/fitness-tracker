2019b3dc53e860f93621918a0e1f5e7690e57b6b
Delvin Lim <delvinlim6414@gmail.com>
2025-09-17 03:52:05 +0800

    feature: added ability for use to take selfie in SummaryView

--- fitness-tracker/fitness-tracker/view/SummaryView.swift
+++ fitness-tracker/fitness-tracker/view/SummaryView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SummaryView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
     @Binding var currentView: AppView
+    @State private var showCamera = false
     private var workout: Workout { workoutManager.lastSessionInMemory }
     
     var body: some View {
@@ -52,12 +53,53 @@ struct SummaryView: View {
             .background(Theme.panelBackground)
             .cornerRadius(12)
             
+            // selfie thumbnail
+            if let fname = workout.selfieFilename,
+               let uiImage = PersistenceManager.shared.loadImage(fileName: fname) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 120)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .shadow(radius: 4)
+                    .padding(.bottom, 8)
+            }
+            
+            // camera button (only if missing)
+            if workout.selfieFilename == nil {
+                Button {
+                    showCamera = true
+                } label: {
+                    Label("Add Selfie", systemImage: "camera")
+                        .bold()
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.accentColor)
+                        .foregroundColor(.white)
+                        .cornerRadius(12)
+                        .shadow(radius: 4)
+                }
+                .padding(.horizontal)
+                .padding(.bottom, 8)
+            }
+            
+            // existing back-home
             Button("Back Home") {
                 currentView = .welcome
-            }.padding()
+            }
+            .padding()
+            
             Spacer()
         }
         .padding()
+        .sheet(isPresented: $showCamera) {
+            CameraView { image in
+                if let fname = PersistenceManager.shared.saveImageToDocuments(image) {
+                    workoutManager.lastSessionInMemory.selfieFilename = fname
+                    PersistenceManager.shared.saveWorkout(workoutManager.lastSessionInMemory)
+                }
+            }
+        }
     }
 }
 
